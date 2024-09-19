6f23829bd7b25b08205e098f9e22c7590571fca0
JonatanHauge <johauge@hotmail.com>
2024-09-19 15:14:41 +0200

    WIP: fixing log like

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import torch.optim as optim
 from time import time
 
 log_npdf = lambda x, m, v: -(x - m) ** 2 / (2 * v) - 0.5 * torch.log(2 * torch.pi * v)
+log_mvnpdf = lambda x, m, v: -0.5 * torch.sum((x - m) ** 2 / v + torch.log(2 * torch.pi * v), dim=1)
+softmax = lambda x: torch.exp(x - torch.max(x, dim=1, keepdim=True)[0]) / torch.sum(torch.exp(x - torch.max(x, dim=1, keepdim=True)[0]), dim=1, keepdim=True)
 
 class VariationalInference(object):
     
@@ -13,57 +15,17 @@ class VariationalInference(object):
         self.name = name
         self.verbose = verbose        
         self.X, self.y, self.ELBO = None, None, None
-    
-        # optimization settings
         self.num_params, self.step_size, self.max_itt = num_params, step_size, max_itt
-        
-        # number of parameters to be optimized is 2*D
         self.num_var_params = 2*self.num_params
         
         # Initialize the variational parameters for the mean-field approximation
-        self.m, self.v = torch.zeros(num_params, requires_grad=True), torch.ones(num_params, requires_grad=True) #/self.num_params
-        #self.lam = self.pack(self.m, self.v) # combine m and v into one vector
-        
-        # prepare optimizer and gradient
+        self.m, self.v = torch.zeros(num_params, requires_grad=True), torch.zeros(num_params, requires_grad=True) #initialize v in log domain   
+        # prepare optimizer
         self.optimizer = optim.Adam(params=[self.m, self.v], lr=step_size)
-        #(initial_param=self.lam, num_params=self.num_var_params, step_size=self.step_size)
-
-        #self.compute_ELBO_gradient = grad(self.compute_ELBO)
-        #self.compute_ELBO_gradient = lambda lam: grad(self.compute_ELBO)(lam)
-        
-    def pack(self, m, v):
-        """ Pack all parameters into one big vector for (unconstrained) optimization as follows: lam = [m, log v] """
-        # Ensure m and v are PyTorch tensors
-        #m = m.clone().detach().requires_grad_(True)
-        #v = v.clone().detach().requires_grad_(True)
-        
-        #lam = torch.zeros(self.num_var_params, dtype=torch.float32, requires_grad=True)
-        #lam[:self.num_params] = m
-        #lam[self.num_params:2*self.num_params] = torch.log(v)
-        print("Are m and v leafs? ", m.is_leaf, v.is_leaf)
-        lam = torch.cat([m, torch.log(v)])
-        #lam.requires_grad_(True)
-        print(lam.is_leaf)
-        return lam
-    
-    def unpack(self, lam):
-        """ Unpack to mean and variance from lam = [m, log-v] """
-        # Ensure lam is a PyTorch tensor
-        lam = torch.tensor(lam, dtype=torch.float32)
-        mean = lam[:self.num_params]
-        var = torch.exp(lam[self.num_params:2*self.num_params])
-        return mean, var
         
     def compute_entropy(self, v=None):
         """ Compute entropy term """
-        #if v is None:
-            # Convert self.lam to a PyTorch tensor if it's not already
-        #    lam = torch.tensor(self.lam, dtype=torch.float32)
-        #    v = torch.exp(lam[self.num_params:2*self.num_params])
-        #else:
-            # Ensure v is a PyTorch tensor
-            #v = torch.tensor(v, dtype=torch.float32)
-        entropy = 0.5 * torch.log(2 * torch.pi * self.v) + 0.5
+        entropy = 0.5 * torch.log(2 * torch.pi * v) + 0.5
         return entropy.sum()  
 
     def generate_posterior_samples(self, num_samples=1000):
@@ -72,46 +34,36 @@ class VariationalInference(object):
         std = torch.sqrt(var)
         samples = torch.normal(mean=mean.unsqueeze(0), std=std.unsqueeze(0).expand(num_samples, -1))
         return samples
-        
-    
 
     def fit(self, X, y, seed=0):
         """ fits the variational approximation q given data (X,y) by maximizing the ELBO using gradient-based methods """ 
         torch.manual_seed(seed)
         self.X, self.y, self.N = X, y, len(X)
         self.ELBO_history, self.m_history, self.v_history = [], [], []
+        self.log_like_history, self.log_prior_history, self.entropy_history = [], [], []
         
         print('Fitting approximation using %s' % self.name)        
         t0 = time()
         for itt in range(self.max_itt):
             
             # evaluate ELBO
-            ELBO = self.compute_ELBO()
+            ELBO, [log_like, prior, entropy] = self.compute_ELBO()
             
             # store current values for plotting purposes
             self.ELBO_history.append(-ELBO.clone().detach().numpy())
             self.m_history.append(self.m.clone().detach().numpy())
             self.v_history.append(torch.exp(self.v.clone().detach()).numpy())
-
-            # compute gradient of ELBO wrt. variational parameters
-            #g = self.compute_ELBO_gradient(self.lam)
-        
-
-            # take gradient step
+            self.log_like_history.append(log_like.clone().detach().numpy())
+            self.log_prior_history.append(prior.clone().detach().numpy())
+            self.entropy_history.append(entropy.clone().detach().numpy())
 
             self.optimizer.zero_grad()
             ELBO.backward()
-            #print gradient
             self.optimizer.step() #SHould update self.lam
-
-            
-            #self.lam = self.optimizer.step(g)
             
             # verbose?
             if self.verbose:
                 if (itt+1) % 250 == 0:
-                    print('m gradient: ', self.m.grad)
-                    print('v gradient: ', self.v.grad)
                     print('\tItt: %5d, ELBO = %3.2f' % (itt, np.mean(self.ELBO_history[-250:])))
         
         t1 = time()
@@ -119,18 +71,20 @@ class VariationalInference(object):
         
         # track quantities through iterations for visualization purposes
         self.ELBO_history = torch.tensor(self.ELBO_history) #since we optimze the negative ELBO
-        #self.lam_history = self.lam_history
-        #self.m = self.lam[:self.num_params]
-        #self.v = torch.exp(self.lam[self.num_params:2*self.num_params])
         self.m_history = np.array(self.m_history)
         self.v_history = np.array(self.v_history)
+        self.log_like_history = np.array(self.log_like_history)
+        self.log_prior_history = np.array(self.log_prior_history)
+        self.entropy_history = np.array(self.entropy_history)
             
         return self
     
 class BlackBoxVariationalInference(VariationalInference):
-    def __init__(self, theta_map, P, log_prior, log_lik, num_params, step_size=1e-2, max_itt=2000, batch_size=None, seed=0, verbose=False, T = 1000):
+    def __init__(self, model, theta_map, P, log_prior, log_lik, num_params, step_size=1e-2, max_itt=2000, batch_size=None, seed=0, verbose=False, T = 1000):
     
         # arguments specific to BBVI
+        self.model = model                  # model
+        self.params = extract_parameters(model) # extract parameters
         self.log_prior = log_prior          # function for evaluating the log prior
         self.log_lik = log_lik              # function for evaluating the log likelihood
         self.batch_size = batch_size        # batch size
@@ -157,41 +111,73 @@ class BlackBoxVariationalInference(VariationalInference):
 
             """
         
-        # unpack variational parameters
-        #m, v  = self.unpack(lam)
-        
         # generate samples from epsilon ~ N(0, 1) and use re-parametrization trick
         epsilon = torch.randn(self.num_params)
-
-        # Compute z_samples using PyTorch tensor operations
-        z_samples = self.m + torch.sqrt(self.v) * epsilon  # shape:  (,K)
-
-        #print('shapes:', z_samples.shape, self.P.T.shape, self.theta_map.shape)
+        z_samples = self.m + torch.sqrt(torch.exp(self.v)) * epsilon  # shape:  (,K)
         w_samples = z_samples @ self.P.T + self.theta_map   # shape: (, D)
-        #w_samples -= - z_samples @ self.P.T
-
-        # prior term (scalar )
         expected_log_prior_term = torch.mean(self.log_prior(z_samples))  # shape: scalar
     
         # batch mode or minibatching?
         if self.batch_size:
             # Use mini-batching
-            
             batch_idx = torch.randperm(self.N)[:self.batch_size]
-
             # Use the indices to create batches
             X_batch = self.X[batch_idx]
             y_batch = self.y[batch_idx]
               
-            expected_log_lik_term = self.N/self.batch_size*self.log_lik(X_batch, y_batch, w_samples)  # shape: scalar
+            expected_log_lik_term = self.N/self.batch_size*self.log_lik(self.model, self.params, X_batch, y_batch, w_samples)  # shape: scalar
         else:
             # No mini-batching
             expected_log_lik_term = self.log_lik(self.X, self.y, w_samples)   # shape: scalar
         
         # compute ELBO
-        #ELBO = 1/self.T * expected_log_lik_term + expected_log_prior_term + self.compute_entropy(self.v)
-        ELBO = 1/self.T * expected_log_lik_term
+        ELBO = 1/self.T * expected_log_lik_term + expected_log_prior_term + self.compute_entropy(torch.exp(self.v))
         
-        return -ELBO # return negative ELBO since we want to maximize and the optimizer minimizes
+        return -ELBO, [1/self.T * expected_log_lik_term, expected_log_prior_term, self.compute_entropy(torch.exp(self.v))]
+
+
+
+def log_like_NN_classification(model, params, X, y, theta):
+    """
+    Implements the log likelihood function for the classification NN with categorical likelihood.
+    S is number of MC samples, N is number of datapoints in likelihood and D is the dimensionality of the model (number of weights).
+
+    Inputs:
+    X              -- Data (np.array of size N x D)
+    y              -- vector of target (np.array of size N)
+    theta_s        -- vector of weights (np.array of size (S, D))
+
+    outputs: 
+    log_likelihood -- Array of log likelihood for each sample in z (np.array of size S)
+     """
+
+    set_weights(params, theta)# Set the weights for the model
+    nll = torch.nn.CrossEntropyLoss(reduction='sum')(model(X), y)
     
+    return -nll
+
+def log_prior_pdf(z, prior_mean=torch.tensor(0), prior_var=torch.tensor(1)):
+    """ Log prior for the weights (assuming Gaussian prior)"""
+    log_prior = torch.sum(log_npdf(z, prior_mean, prior_var))
+    return log_prior
+
+def extract_parameters(model):
+    params = []	
+    for module in model.modules():	
+        for name in list(module._parameters.keys()):	
+            if module._parameters[name] is None:	
+                continue	
+            param = module._parameters[name]	
+            params.append((module, name, param.size()))	
+            module._parameters.pop(name)	
+    return params
+
+def set_weights(params, w):	
+    offset = 0
+    for module, name, shape in params:
+        size = np.prod(shape)	       
+        value = w[offset:offset + size]
+        setattr(module, name, value.view(shape))	
+        offset += size
+
 
